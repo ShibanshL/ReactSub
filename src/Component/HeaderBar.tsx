1f51a16cbd72ac3ec2a6e199c6d6c0f0cbbc44ab
ShibanshL <shibansh.w@gmail.com>
2023-07-20 01:50:42 +0530

    fifth commit

--- src/Component/HeaderBar.tsx
+++ src/Component/HeaderBar.tsx
@@ -12,7 +12,7 @@ function HeaderBar() {
     <>
       <div className="HeaderBar">
         <img src={Left} alt="" />
-        <label htmlFor="#">BitCoin Wallet</label>
+        <label htmlFor="#">BitCoinWallet</label>
         <div>
           <img src={ThreeDot} className="ThreeDot" alt="" />
         </div>
