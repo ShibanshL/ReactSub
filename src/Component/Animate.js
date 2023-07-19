80d8529c123fa0c8258046fed8c6006eda7476cd
ShibanshL <shibansh.w@gmail.com>
2023-07-20 01:41:52 +0530

    fourth commit : touch

--- src/Component/Animate.js
+++ src/Component/Animate.js
@@ -10,7 +10,7 @@ const collapseDiv = () => {
 
   let timeFrame = document.querySelectorAll(".TickerTimeFrame button");
 
-  buttonCollapse.addEventListener("click", () => {
+  buttonCollapse.addEventListener("touchstart", () => {
     i++;
 
     if (i % 2 != 0) {
